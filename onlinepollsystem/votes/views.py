7b5fc5895da17a4ffe38c4324a496eaf97f537f2
Favour Idowu <favouridowu02@gmail.com>
2025-09-27 14:37:54 +0100

    feat: implement the Votes views

--- onlinepollsystem/votes/views.py
+++ onlinepollsystem/votes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Vote
+from .serializers import VoteSerializer
+
+class VoteViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet for viewing user's votes"""
+    serializer_class = VoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Vote.objects.filter(user=self.request.user).select_related('poll', 'option', 'user')
